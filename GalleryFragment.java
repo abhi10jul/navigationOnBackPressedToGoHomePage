e394e62c63ed3ca01fc17534ba3cee1474fdb280
Abhishek srivastava <40688827+abhi10jul@users.noreply.github.com>
2018-07-18 14:10:24 +0530

    navigationOnBackPressedToGoHomePage
    NavigationBar click menu and replace other fragment and onBackPressed to back to the homepage and then exit the app and show the notification when user click one click back button and show the message press again to exit to handle the back pressed 2sec delayed and when User is homepage to exit the app with notifying toast

---
+++ GalleryFragment.java
@@ -0,0 +1,26 @@
+package www.abhiandroidknowledge.blogspot.com.navigationbackpressedtogohomepagefragement;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by www.abhiandroidknowledge.blogspot.com on 18/7/18.
+ */
+
+public class GalleryFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.common_fragment, container, false);
+        TextView tv = (TextView) view.findViewById(R.id.textView);
+        tv.setText("Gallery Fragment");
+        return view;
+    }
+}
